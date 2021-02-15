2ac7b9ae79ae43b323d9fd7cc54e68235dfb79bc
Edeiver <edbarranco4@gmail.com>
2021-02-14 23:15:13 -0500

    Add files and styles

---
+++ src/components/LoginForm.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const LoginForm = () => {
+    return (
+        <View>
+            <Text>LoginForm</Text>
+        </View>
+    )
+}
+
+export default LoginForm
+
+const styles = StyleSheet.create({})
---
+++ src/styles/index.js
@@ -0,0 +1,4 @@
+export const COLORS = {
+    mainBlack: '#211f2b',
+    mainGray: '#eff0f2'
+}
